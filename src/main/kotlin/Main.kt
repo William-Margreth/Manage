d5ca50e775cca3123319fcab11ac4273f4997406
William-Margareth <williammargarethpalllanjo@gmail.com>
2023-02-02 07:47:59 +0300

    Variabless

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var fullname = "magreth w"
+    var age = 21
+    var phonenumber = 0673185721
+    var weightInKg = 62
+    var isTanzanian: Boolean True
+    var notKenya: Boolean False
+
+    println(isTanzanian)
+
+
+
+}
+
+
